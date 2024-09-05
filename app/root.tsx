7d6ae1c40d4a27117ddab0a7bdfbe7bf65cd692b
gabrielzepeda1 <gabrielzepeda846@gmail.com>
2024-09-05 15:30:26 -0600

    Revert "removing dark mode script."
    This reverts commit 208d2299d1ee4975024b838be0b2d76926fbeea2.

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import "./tailwind.css";
+import { ThemeModeScript } from "flowbite-react";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -15,6 +16,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <ThemeModeScript />
       </head>
       <body>
         {children}
