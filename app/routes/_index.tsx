728d351b01fec1f98b52da686b804a0205e2cff6
gabrielzepeda1 <gabrielzepeda846@gmail.com>
2024-09-22 21:18:15 -0600

    start to search from landing

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,8 @@
-import { type MetaFunction } from "@remix-run/node";
+import {
+  type MetaFunction,
+  LoaderFunctionArgs,
+  redirect,
+} from "@remix-run/node";
 import Hero from "~/components/Hero";
 import MainFeatures from "~/components/MainFeatures";
 import Testimonials from "~/components/Testimonials";
@@ -10,11 +14,29 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function loader({ request }: LoaderFunctionArgs) {
+  const url = new URL(request.url);
+  const searchTerm = url.searchParams.get("searchTerm");
+
+  if (searchTerm) {
+    return redirect(`/app/?searchTerm=${searchTerm}`);
+  }
+  return null;
+}
+
+// export async function action({ request }: ActionFunctionArgs) {
+//   const formData = await request.formData();
+//   const searchParam = formData.get("searchTerm");
+
+//   return json({ searchParam: searchParam });
+// }
+
 export default function Landing() {
+  // const actionData = useActionData<typeof action>();
+  // console.log(actionData);
   return (
     <>
       <Hero />
-      {/* Features */}
       <MainFeatures />
       <Testimonials />
     </>
--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,11 +1,7 @@
-import {
-  ActionFunctionArgs,
-  json,
-  LoaderFunctionArgs,
-  type MetaFunction,
-} from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
+import { json, LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import GameCard from "~/components/GameCard";
+import SearchForm from "~/components/SearchForm";
 import { getGames } from "~/lib/api";
 
 export const meta: MetaFunction = () => {
@@ -19,20 +15,12 @@ export const meta: MetaFunction = () => {
 export async function loader({ request }: LoaderFunctionArgs) {
   const url = new URL(request.url);
   const searchTerm = url.searchParams.get("searchTerm");
-
-  const query = searchTerm
-    ? `search "${searchTerm}"; fields name, summary, cover.*; limit 20;`
-    : "fields name, summary, cover.*; limit 500;";
-
-  const data = await getGames(query);
-
-  return json({ data, searchTerm });
-}
-
-export async function action({ request }: ActionFunctionArgs) {
-  const formData = await request.formData();
-  const searchTerm = formData.get("search");
-  return json({ searchTerm: searchTerm });
+  if (searchTerm) {
+    const query = `search "${searchTerm}"; fields name, summary, cover.*; limit 20;`;
+    const data = await getGames(query);
+    return json({ data: data, searchTerm: searchTerm });
+  }
+  return json({ data: [], searchTerm: null });
 }
 
 export default function Index() {
@@ -48,47 +36,7 @@ export default function Index() {
   return (
     <>
       <div className="mb-3 py-3">
-        <Form method="get" className="max-w-md mx-auto">
-          <label
-            htmlFor="default-search"
-            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-          >
-            Search
-          </label>
-          <div className="relative">
-            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-              <svg
-                className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
-            </div>
-            <input
-              type="search"
-              name="searchTerm"
-              id="default-search"
-              className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search Games..."
-              required
-            />
-            <button
-              type="submit"
-              className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Search
-            </button>
-          </div>
-        </Form>
+        <SearchForm searchTerm={searchTerm} />
         {searchTerm && (
           <p className="text-sm lg:text-lg text-gray-900 dark:text-gray-200 mt-1 ml-1">
             Showing results for...{" "}
@@ -97,9 +45,13 @@ export default function Index() {
         )}
       </div>
       <div className="grid grid-cols-1 md:grid-cols-4 xl:grid-cols-6 gap-6 space-y-3 lg:px-3">
-        {data.map((game: any) => (
-          <GameCard key={game.id} game={game} />
-        ))}
+        {searchTerm !== null ? (
+          data.map((game: any) => <GameCard key={game.id} game={game} />)
+        ) : (
+          <span className="text-xl text-gray-900 dark:text-white">
+            Wow! You haven't searched for any games yet
+          </span>
+        )}
       </div>
     </>
   );
